2c7285849bea57b02af7f37c8937c444069741a1
bandrawy <a.Elbandrawy@boltsmartsolutions.com>
2020-12-29 15:35:19 +0200

    ..
    GET LATEST

--- TopSaloon.API/Startup.cs
+++ TopSaloon.API/Startup.cs
@@ -36,7 +36,9 @@ namespace TopSaloon.API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Server=138.201.213.62\\SQL2019; Database=TOPSALONTEST;User ID=sa;Password=P@$$w0rd;"));
+            // services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Server=138.201.213.62\\SQL2019; Database=TOPSALONTEST;User ID=sa;Password=P@$$w0rd;"));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Server=BOLT-BANDRAWY; Database=TOPSALOON;User ID=sa;Password=P@ssw0rd;"));
+
 
             //services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Server = 138.201.213.62\\SQL2019; Database = TOPSALONTEST; User ID = sa; password = P@$$w0rd; ", builder =>
             //{
